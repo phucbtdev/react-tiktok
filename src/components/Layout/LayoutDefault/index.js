5ca3cf5ceb51e1bc400db9a38fb61c6c67e21ec3
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-09-23 16:48:21 +0700

    Generate default layout

--- src/components/Layout/LayoutDefault/index.js
+++ src/components/Layout/LayoutDefault/index.js
@@ -1,14 +1,21 @@
 import Header from "~/components/Layout/components/Header";
 import Sidebar from "~/components/Layout/components/Sidebar";
 
+import styles from './Layout.module.scss';
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles)
+
 function LayoutDefault({ children }) {
-      return <>
-            <Header></Header>
-            <div className="container">
-                  <Sidebar></Sidebar>
-                  <div className="content">{children}</div>
+      return (
+            <div className={cx('wrapper')}>
+                  <Header></Header>
+                  <div className={cx('container')}>
+                        <Sidebar></Sidebar>
+                        <div className={cx('content')} >{children}</div>
+                  </div>
             </div>
-      </>;
+      );
 }
 
 export default LayoutDefault;
